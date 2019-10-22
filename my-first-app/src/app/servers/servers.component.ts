bd7d34b6d783c227dd30a7b132809d6e687049bf
Florian Lapolla <awflo1306@gmx.de>
2019-10-22 17:21:24 +0200

    2.21 Fully Understanding the Component Selector

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // selector: '[app-servers]', Selector is now a CSS-Attribute
+  // selector: '.app-servers', // Selector is now a CSS-Class
+  selector: 'app-servers', // Der Selektor sollte in der Regel bei Components immer ien HTML-Tag sein. Das obige kann man machen sollte man aber nicht
   template: `
   <app-server></app-server>
   <app-server></app-server>`, // Backtick, um über mehrere Zeilen schreiben zu können
