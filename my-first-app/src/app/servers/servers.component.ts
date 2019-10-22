39a9a3d554d58052a177fa48193a43eaa0898fcd
Florian Lapolla <awflo1306@gmx.de>
2019-10-22 16:49:04 +0200

    2.19 Working with Component Templates

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  template: `
+  <app-server></app-server>
+  <app-server></app-server>`, // Backtick, um über mehrere Zeilen schreiben zu können
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
